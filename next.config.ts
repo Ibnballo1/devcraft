1e2b6c460078b40b54e970872e25b4d3e97d32a5
Abdullateef Bello <belloabdullateef035@gmail.com>
2025-04-27 23:01:14 +0100

    Add a detailed post

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      new URL("https://picsum.photos/seed/MXjcp/2400/276?blur=3"),
+    ],
+  },
 };
 
 export default nextConfig;

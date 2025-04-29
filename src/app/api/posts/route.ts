9ec80499229572549b8d0fdcf807d1bfadd0f132
Abdullateef Bello <belloabdullateef035@gmail.com>
2025-04-29 19:36:10 +0100

    update api route for posts

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -26,38 +26,38 @@ export async function GET() {
   }
 }
 
-// export async function POST(req: Request) {
-//   try {
-//     const body = await req.json();
-//     const { title, content, coverImageUrl, authorId } = body;
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { title, content, coverImageUrl, authorId } = body;
 
-//     if (!title || !content || !authorId) {
-//       return NextResponse.json(
-//         { message: "Missing required fields" },
-//         { status: 400 }
-//       );
-//     }
+    if (!title || !content || !authorId) {
+      return NextResponse.json(
+        { message: "Missing required fields" },
+        { status: 400 }
+      );
+    }
 
-//     const slug = `${slugify(title, { lower: true, strict: true })}-${
-//       randomUUID().split("-")[0]
-//     };`;
+    const slug = `${slugify(title, { lower: true, strict: true })}-${
+      randomUUID().split("-")[0]
+    };`;
 
-//     const post = await prisma.post.create({
-//       data: {
-//         title,
-//         content,
-//         slug,
-//         coverImageUrl,
-//         authorId,
-//         readTime: Math.ceil(content.split(" ").length / 200),
-//       },
-//     });
-//     return NextResponse.json(post, { status: 201 });
-//   } catch (error) {
-//     console.log("error creating post", error);
-//     return NextResponse.json(
-//       { message: "Internal server error" },
-//       { status: 500 }
-//     );
-//   }
-// }
+    const post = await prisma.post.create({
+      data: {
+        title,
+        content,
+        slug,
+        coverImageUrl,
+        authorId,
+        readTime: Math.ceil(content.split(" ").length / 200),
+      },
+    });
+    return NextResponse.json(post, { status: 201 });
+  } catch (error) {
+    console.log("error creating post", error);
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}

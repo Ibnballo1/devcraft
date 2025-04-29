7ec46182b141f3264fdb8002227f139f8750624b
Abdullateef Bello <belloabdullateef035@gmail.com>
2025-04-29 19:34:46 +0100

    Add footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="mt-12 border-t pt-8 pb-6 text-center text-gray-500 text-sm">
+      © {new Date().getFullYear()} DevCraft. All rights reserved.
+    </footer>
+  );
+}

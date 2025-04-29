4980cf78fad4900104ab5c8961eb73a4f0a447a8
Abdullateef Bello <belloabdullateef035@gmail.com>
2025-04-29 19:37:41 +0100

    add absolute url

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,15 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       new URL("https://picsum.photos/seed/MXjcp/2400/276?blur=3"),
+      new URL("https://picsum.photos/seed/bRZezVrv/2014/1251?grayscale&blur=2"),
+      new URL("https://picsum.photos/seed/czDDZ/2521/1290?blur=6"),
+      new URL("https://picsum.photos/seed/tZRWOuE/3586/2727?blur=9"),
+      new URL("https://picsum.photos/seed/YV1mt/1954/2072?grayscale&blur=10"),
+      new URL("https://picsum.photos/seed/HMGg9TIwiy/491/515?grayscale&blur=3"),
+      new URL("https://picsum.photos/seed/HJheb/1899/2432?blur=7"),
+      new URL("https://picsum.photos/seed/nkQQBs/1245/3148?grayscale&blur=2"),
+      new URL("https://picsum.photos/seed/4SgvBL/2306/1911?blur=9"),
+      // new URL("https://picsum.photos/seed/0yI9NCw8F/1884/2819"),
     ],
   },
 };
